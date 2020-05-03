e25f47d000b49e76c078bdebf3b6bf45164c208e
Sian Hou <sianhou1987@gmail.com>
2020-05-03 22:49:44 +0800

    ie

--- os/blog_os/src/main.rs
+++ os/blog_os/src/main.rs
@@ -29,7 +29,7 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash!");
-    loop {}
+    blog_os::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -37,7 +37,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    blog_os::hlt_loop();
 }
 
 #[cfg(test)]
